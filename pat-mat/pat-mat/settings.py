8cd09cb6ce9d36af2fa41b2857980147a3fd7e8d
Mohammad <mohammad.ghorbani@hotmail.com>
2025-01-08 23:02:55 +0330

    fix: adjust STATIC_ROOT configuration in settings.py

--- pat-mat/pat-mat/settings.py
+++ pat-mat/pat-mat/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 if not DEBUG:
     MEDIA_URL = "/static/media/"
